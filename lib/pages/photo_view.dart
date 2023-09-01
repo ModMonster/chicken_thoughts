21830bf98a662c670a9a9ff4c4f7ad32ca702d67
ModMonster <modmonster2060@gmail.com>
2023-09-01 14:28:57 -0400

    Allow scrolling with arrow keys

--- lib/pages/photo_view.dart
+++ lib/pages/photo_view.dart
@@ -2,6 +2,7 @@ import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chicken_thoughts_notifications/pages/history.dart';
 import 'package:chicken_thoughts_notifications/widgets/error_fetching.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
@@ -31,13 +32,34 @@ class _PhotoViewPageState extends State<PhotoViewPage> {
       initialPage: widget.initialPage
     );
 
+    ServicesBinding.instance.keyboard.addHandler((event) => scroll(event));
+
     super.initState();
+  }
+
+  bool scroll(KeyEvent event) {
+    final key = event.logicalKey;
+
+    if (event is KeyDownEvent) {
+      if (key == LogicalKeyboardKey.arrowLeft) {
+        pageController.previousPage(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOutCubic
+        );
+      } else if (key == LogicalKeyboardKey.arrowRight) {
+        pageController.nextPage(
+          duration: Duration(milliseconds: 500),
+          curve: Curves.easeInOutCubic
+        );
+      }
+    }
+
+    return false;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      extendBodyBehindAppBar: true,
       appBar: AppBar(
         elevation: 4,
         title: ListTile(
@@ -56,7 +78,11 @@ class _PhotoViewPageState extends State<PhotoViewPage> {
         backgroundDecoration: BoxDecoration(color: Theme.of(context).colorScheme.background),
         builder: (BuildContext context, int index) {
           return PhotoViewGalleryPageOptions.customChild(
-            child: CachedNetworkImage(imageUrl: chickyThoughts[index].url, errorWidget: (context, idk, progress) => ErrorFetching()),
+            child: CachedNetworkImage(
+              imageUrl: chickyThoughts[index].url,
+              errorWidget: (context, idk, progress) => ErrorFetching(),
+              progressIndicatorBuilder: (context, idk, progress) => Center(child: CircularProgressIndicator()),
+            ),
             minScale: PhotoViewComputedScale.contained * 0.8,
             maxScale: PhotoViewComputedScale.covered * 1.8,
             heroAttributes: index == widget.initialPage? widget.heroTag == null? null :
