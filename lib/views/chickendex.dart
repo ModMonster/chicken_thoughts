cec538afd2a0582f9aca2576f6a234f363b71a4b
ModMonster <modmonster2060@gmail.com>
2026-03-02 12:29:06 -0500

    work in progress chickendex changes

--- lib/views/chickendex.dart
+++ lib/views/chickendex.dart
@@ -1,13 +1,7 @@
 import 'package:chicken_thoughts_notifications/data/season.dart';
-import 'package:chicken_thoughts_notifications/data/vibrate.dart';
 import 'package:chicken_thoughts_notifications/net/database_manager.dart';
-import 'package:chicken_thoughts_notifications/pages/chickendex_image_expanded.dart';
-import 'package:chicken_thoughts_notifications/views/coming_soon.dart';
-import 'package:chicken_thoughts_notifications/widgets/chickendex_grid_image.dart';
-import 'package:chicken_thoughts_notifications/widgets/chickendex_locked.dart';
+import 'package:chicken_thoughts_notifications/views/chickendex/chickendex_tab.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
-import 'package:hive_ce/hive.dart';
 import 'package:loading_indicator_m3e/loading_indicator_m3e.dart';
 
 class ChickendexView extends StatefulWidget {
@@ -18,137 +12,96 @@ class ChickendexView extends StatefulWidget {
 }
 
 class _ChickendexViewState extends State<ChickendexView> {
-  final Future<Season> _future = DatabaseManager.getDefaultSeason();
+  final Future<List<Season>> _seasonListFuture = DatabaseManager.getSeasonList();
+  final List<GlobalKey> keys = [];
 
   @override
   Widget build(BuildContext context) {
-    final Box box = Hive.box("chickendex");
-
     return Scaffold(
       body: FutureBuilder(
-        future: _future,
+        future: _seasonListFuture,
         builder: (context, snapshot) {
           if (!snapshot.hasData || snapshot.data == null) {
             return Center(
               child: LoadingIndicatorM3E()
             );
           }
-      
-          return DefaultTabController(
-            length: 3,
-            child: NestedScrollView(
-              headerSliverBuilder: (context, innerBoxIsScrolled) {
-                return [SliverAppBar(
-                  title: const Text("Chickendex"),
-                  pinned: true,
-                  snap: true,
-                  floating: true,
-                  bottom: TabBar(
-                    tabs: [
-                      Tab(
-                        text: "Normal",
-                        icon: Icon(Icons.photo),
+
+          // Find default season, move to top
+          List<Season> seasons = snapshot.data!;
+          int defaultIndex = 0;
+          for (int i = 0; i < seasons.length; i++) {
+            if (seasons[i].imagePrefix == null) {
+              defaultIndex = i;
+              break;
+            }
+          }
+          Season defaultSeason = seasons[defaultIndex];
+          seasons.removeAt(defaultIndex);
+          seasons.insert(0, defaultSeason);
+
+          return NestedScrollView(
+            headerSliverBuilder: (context, innerBoxIsScrolled) {
+              return [SliverAppBar(
+                title: const Text("Chickendex"),
+                pinned: true,
+                snap: true,
+                floating: true,
+                bottom: PreferredSize(
+                  preferredSize: Size.fromHeight(40),
+                  child: Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(8.0, 8.0, 0.0, 8.0),
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: snapshot.data!.length,
+                        itemBuilder: (context, index) {
+                          return Padding(
+                            padding: const EdgeInsets.only(right: 8.0),
+                            child: ActionChip(
+                              label: Text(snapshot.data![index].displayName?? "Normal"),
+                              onPressed: () {
+                                final ctx = keys[index].currentContext;
+                                if (ctx == null) return;
+                                Scrollable.ensureVisible(ctx);
+                              },
+                            ),
+                          );
+                        },
                       ),
-                      Tab(
-                        text: "Seasonal",
-                        icon: Icon(Icons.ac_unit),
-                      ),
-                      Tab(
-                        text: "Unlocked",
-                        icon: Icon(Icons.lock_open),
-                      )
-                    ],
+                    ),
                   ),
-                  actions: [
-                    if (MediaQuery.of(context).size.width <= 600) IconButton(
-                      onPressed: () {
-                        Navigator.pushNamed(context, "/settings");
-                      },
-                      icon: Icon(Icons.settings),
-                      tooltip: "Settings",
-                    )
-                  ],
-                )];
-              },
-              body: TabBarView(
-                children: [
-                  Column(
+                ),
+                actions: [
+                  if (MediaQuery.of(context).size.width <= 600) IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, "/settings");
+                    },
+                    icon: Icon(Icons.settings),
+                    tooltip: "Settings",
+                  )
+                ],
+              )];
+            },
+            body: ListView(
+              children: List<Widget>.generate(
+                snapshot.data!.length, (index) {
+                  Season season = snapshot.data![index];
+                  GlobalKey key = GlobalKey();
+                  keys.add(key);
+
+                  return Column(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      ListTile(
-                        title: Text(
-                          "${box.length}/${snapshot.data!.imageCount} unlocked"
-                        ),
-                        trailing: OutlinedButton.icon(
-                          onPressed: () {
-                            Vibrate.tap();
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => ChickendexImageExpandedPage()));
-                          },
-                          icon: Icon(Icons.browse_gallery),
-                          label: Text("View all"),
-                        ),
-                      ),
-                      AnimationLimiter(
-                        child: Expanded(
-                          child: LayoutBuilder(
-                            builder: (context, constraints) {
-                              final double availableWidth = constraints.maxWidth;
-                              const double size = 96.0;
-                              final int crossAxisCount = (availableWidth / size).floor();
-                          
-                              return GridView.builder(
-                                itemCount: snapshot.data!.imageCount,
-                                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                                  crossAxisCount: crossAxisCount,
-                                  mainAxisSpacing: 8,
-                                  crossAxisSpacing: 8
-                                ),
-                                padding: EdgeInsets.all(8),
-                                itemBuilder: (context, inp) {
-                                  int index = inp + 1;
-                                  int imageCount = box.get(index.toString(), defaultValue: 0);
-                                          
-                                  // We haven't seen it yet; locked!
-                                  if (imageCount == 0) {
-                                    return AnimationConfiguration.staggeredGrid(
-                                      position: inp,
-                                      duration: const Duration(milliseconds: 375),
-                                      columnCount: crossAxisCount,
-                                      child: ScaleAnimation(
-                                        child: FadeInAnimation(
-                                          child: ChickendexLocked(index.toString())
-                                        )
-                                      )
-                                    );
-                                  }
-                                  
-                                  // We have seen it; show it!
-                                  return AnimationConfiguration.staggeredGrid(
-                                    position: inp,
-                                    duration: const Duration(milliseconds: 375),
-                                    columnCount: crossAxisCount,
-                                    child: ScaleAnimation(
-                                      child: FadeInAnimation(
-                                        child: ChickendexGridImage(imageCount > 1? "$index.1" : index.toString())
-                                      )
-                                    )
-                                  );
-                                }
-                              );
-                            }
-                          ),
-                        ),
-                      ),
+                      ChickendexTabView(season, key: key)
                     ],
-                  ),
-                  ComingSoonView("seasonal"),
-                  ComingSoonView("all")
-                ],
+                  );
+                }
               )
-            ),
+            )
           );
         }
-      ),
+      )
     );
   }
 }
---
+++ lib/views/chickendex/chickendex_normal.dart
@@ -0,0 +1,16 @@
+import 'package:chicken_thoughts_notifications/views/chickendex.dart';
+import 'package:flutter/material.dart';
+
+class ChickendexNormal extends ChickendexView {
+  const ChickendexNormal({super.key});
+
+  @override
+  State<ChickendexNormal> createState() => _ChickendexNormalState();
+}
+
+class _ChickendexNormalState extends State<ChickendexNormal> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
