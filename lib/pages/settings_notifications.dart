67fb49db645bd916aea54ef8434f6530ea27d93a
ModMonster <modmonster2060@gmail.com>
2026-03-05 08:50:29 -0500

    Fix #36 - add description to android notification settings option and add a streak reminder time setting

--- lib/pages/settings_notifications.dart
+++ lib/pages/settings_notifications.dart
@@ -29,7 +29,7 @@ class _SettingsNotificationPageState extends State<SettingsNotificationPage> {
             sliver: SliverList.list(
               children: [
                 ListTile(
-                  title: Text("Notification time"),
+                  title: Text("Daily notification time"),
                   subtitle: Text(box.get("notifications.time", defaultValue: TimeOfDay(hour: 7, minute: 0)).format(context)),
                   leading: Icon(Icons.access_time_outlined),
                   onTap: () async {
@@ -43,7 +43,22 @@ class _SettingsNotificationPageState extends State<SettingsNotificationPage> {
                   }
                 ),
                 ListTile(
+                  title: Text("Streak reminder time"),
+                  subtitle: Text(box.get("notifications.streak_reminder_time", defaultValue: TimeOfDay(hour: 20, minute: 0)).format(context)),
+                  leading: Icon(Icons.local_fire_department_outlined),
+                  onTap: () async {
+                    TimeOfDay? chosenTime = await showTimePicker(
+                      context: context,
+                      initialTime: box.get("notifications.streak_reminder_time", defaultValue: TimeOfDay(hour: 20, minute: 0)),
+                    );
+                    if (chosenTime == null) return;
+                    box.put("notifications.streak_reminder_time", chosenTime);
+                    await NotificationManager.scheduleDailyNotification();
+                  }
+                ),
+                ListTile(
                   title: Text("Android notification settings"),
+                  subtitle: Text("Toggle notifications or adjust how they are delivered"),
                   leading: Icon(Icons.open_in_new_outlined),
                   onTap: () {
                     AppSettings.openAppSettings(type: AppSettingsType.notification);
