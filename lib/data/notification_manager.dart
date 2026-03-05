b6f48ade6aea1e0422bc290b4fa56decbe91b1d9
ModMonster <modmonster2060@gmail.com>
2026-03-05 10:13:19 -0500

    The streak reminder notification (#35) may be done!

--- lib/data/notification_manager.dart
+++ lib/data/notification_manager.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:chicken_thoughts_notifications/pages/settings_color.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -10,6 +12,10 @@ class NotificationManager {
   static const dailyChannelId = "daily";
   static const dailyChannelName = "Daily reminders";
   static const dailyChannelDescription = "Reminders every morning for new Chicken Thoughts";
+
+  static const streakReminderChannelId = "streak_reminders";
+  static const streakReminderChannelName = "Streak reminders";
+  static const streakReminderChannelDescription = "Notifications for if you are going to lose your streak";
 
   static final FlutterLocalNotificationsPlugin notificationsPlugin = FlutterLocalNotificationsPlugin();
 
@@ -30,7 +36,7 @@ class NotificationManager {
 
     await androidNotifications.requestNotificationsPermission();
 
-    // create notifications channel
+    // create daily notifications channel
     androidNotifications.createNotificationChannel(
       AndroidNotificationChannel(
         dailyChannelId,
@@ -40,10 +46,65 @@ class NotificationManager {
       )
     );
 
+    // create streak reminder notifications channel
+    androidNotifications.createNotificationChannel(
+      AndroidNotificationChannel(
+        streakReminderChannelId,
+        streakReminderChannelName,
+        description: streakReminderChannelDescription,
+        importance: Importance.high,
+      )
+    );
+
     initializeTimeZones();
     final TimezoneInfo localTimeZone = await FlutterTimezone.getLocalTimezone();
     setLocalLocation(getLocation(localTimeZone.identifier));
     await scheduleDailyNotification();
+    await scheduleStreakReminderNotification();
+  }
+
+  static Future<void> scheduleStreakReminderNotification() async {
+    notificationsPlugin.cancel(id: 1); // cancel existing warning notifications
+
+    int streak = Hive.box("settings").get("streak", defaultValue: 0);
+    if (streak == 0) return;
+
+    final TimeOfDay time = Hive.box("settings").get("notifications.streak_reminder_time", defaultValue: TimeOfDay(hour: 20, minute: 0));
+
+    final tomorrow = TZDateTime.now(local).add(Duration(days: 1));
+    var scheduledDate = TZDateTime(
+      local,
+      tomorrow.year,
+      tomorrow.month,
+      tomorrow.day,
+      time.hour,
+      time.minute
+    );
+
+    List<String> chickenstreakReminderMessages = [
+      "Don't let your Chickenstreak fizzle out!",
+      "Your Chickenstreak is in danger!",
+      "Don't lose your Chickenstreak!",
+    ];
+
+    await notificationsPlugin.zonedSchedule(
+      id: 1,
+      scheduledDate: scheduledDate,
+      title: chickenstreakReminderMessages[Random().nextInt(chickenstreakReminderMessages.length)],
+      body: "View today's Chicken Thought so you don't lose your $streak day streak!",
+      androidScheduleMode: AndroidScheduleMode.inexactAllowWhileIdle,
+      notificationDetails: NotificationDetails(
+        android: AndroidNotificationDetails(
+          streakReminderChannelId,
+          streakReminderChannelName,
+          icon: "ic_notification",
+          channelDescription: streakReminderChannelDescription,
+          importance: Importance.high,
+          priority: Priority.high,
+          color: colors[Hive.box("settings").get("color", defaultValue: 0)].color
+        )
+      ),
+    );
   }
 
   static Future<void> scheduleDailyNotification() async {
