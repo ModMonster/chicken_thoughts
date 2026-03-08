ed390e8d7e4cb8ddb2e874e915e020a8f2a19e55
ModMonster <modmonster2060@gmail.com>
2026-03-08 16:50:53 -0400

    Tweaked some values with rotation animation after testing on real device

--- lib/widgets/tilted_avatar.dart
+++ lib/widgets/tilted_avatar.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
@@ -13,53 +11,37 @@ class Tilted extends StatefulWidget {
 }
 
 class _TiltedState extends State<Tilted> {
-  double _rotX = 0.0;
-  double _rotY = 0.0;
-
   double _neutralX = 0.0;
   double _neutralY = 0.0;
   bool _calibrated = false;
 
-  StreamSubscription? _accelerometorSub;
-
-  @override
-  void initState() {
-    super.initState();
-
-    // Listen to accelerometer for rotation
-    if (kIsWeb) return;
-    _accelerometorSub = accelerometerEventStream().listen((event) {
-      if (!mounted) return;
-
-      if (!_calibrated) {
-        _calibrated = true;
-        _neutralX = event.x;
-        _neutralY = event.y;
-      }
-
-      print("${event.x}, ${event.y}");
-      setState(() {
-        _rotX = ((event.y - _neutralY) / 10).clamp(-0.3, 0.3);
-        _rotY = (-(event.x - _neutralX) / 10).clamp(-0.3, 0.3);
-      });
-    });
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    _accelerometorSub?.cancel();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return !kIsWeb? Transform(
-      alignment: Alignment.center,
-      transform: Matrix4.identity()
-        ..setEntry(3, 2, 0.001)
-        ..rotateX(_rotX)
-        ..rotateY(_rotY),
-      child: widget.child
+    return !kIsWeb? StreamBuilder(
+      stream: accelerometerEventStream(),
+      builder: (context, snapshot) {
+        double rotX = 0.0;
+        double rotY = 0.0;
+        if (snapshot.hasData && snapshot.data != null) {
+          if (!_calibrated) {
+            _calibrated = true;
+            _neutralX = snapshot.data!.x;
+            _neutralY = snapshot.data!.y;
+          }
+
+          rotX = ((snapshot.data!.y - _neutralY) / 8).clamp(-0.4, 0.4);
+          rotY = (-(snapshot.data!.x - _neutralX) / 15).clamp(-0.4, 0.4);
+        }
+
+        return Transform(
+          alignment: Alignment.center,
+          transform: Matrix4.identity()
+            ..setEntry(3, 2, 0.001)
+            ..rotateX(rotX)
+            ..rotateY(rotY),
+          child: widget.child
+        );
+      }
     ) : widget.child;
   }
 }
